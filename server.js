5af9bd2f5fa54d14b7444277a3c4cb368a262dc9
clarkedav <clarkdav282@gmail.com>
2026-02-07 05:40:57 +0100

    changes made

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 /* ***********************
  * Require Statements
  *************************/
+const session = require("express-session")
+const pool = require('./database/')
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
@@ -15,6 +17,24 @@ const utilities = require("./utilities/")
 //const static = require("./routes/static")
 
 app.use(express.static("public"))
+
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+
+
 
 /* ***********************
  * View Engine and Templates
